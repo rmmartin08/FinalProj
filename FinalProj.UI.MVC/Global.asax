be0bf4a2da4eb2461b6a8129f0c47efce7e1c434
rmmartin08 <rmmartin08@gmail.com>
2020-06-01 10:06:34 -0500

    Add project files.

---
+++ FinalProj.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalProj.UI.MVC.MvcApplication" Language="C#" %>
---
+++ FinalProj.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProj.UI.MVC.Startup))]
+namespace FinalProj.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
