79a87732607b86e8b4176695e6741662dc07606a
rmmartin08 <rmmartin08@gmail.com>
2020-06-05 11:44:38 -0500

    udpated css, added script to remove main content section from index added login to index

--- FinalProj.UI.MVC/App_Start/BundleConfig.cs
+++ FinalProj.UI.MVC/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace FinalProj.UI.MVC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                
+                ));
         }
     }
 }
